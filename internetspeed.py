59b8463db0d6a83c3b9b8499ed5e2d6de154dce8
Robert <54269637+R-udren@users.noreply.github.com>
2023-10-12 17:56:40 +0300

    added byte unit formatting

--- internetspeed.py
+++ internetspeed.py
@@ -4,11 +4,20 @@ from rich.console import Console
 from rich import print
 
 
+def format_bytes(raw_bytes):
+    units = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb']
+    i = 0
+    while raw_bytes >= 1024:
+        raw_bytes /= 1024
+        i += 1
+    return f'{raw_bytes:.2f} {units[i]}'
+
+
 # Function to check ping
 def check_ping(host):
-    response_time = ping(host)
+    response_time = round(ping(host), 3)
     if response_time is not None:
-        return f'[green]Ping to {host} is {response_time:0.3f} ms[/green]'
+        return f'[green]Ping to {host} is {response_time} ms[/green]'
     else:
         return f'[red]Could not reach {host}[/red]'
 
@@ -18,11 +27,11 @@ def check_speed():
     st = speedtest.Speedtest()
     st.get_best_server()  # Find the best server
 
-    download_speed = st.download() / 10 ** 6  # Convert to Mbps
-    upload_speed = st.upload() / 10 ** 6  # Convert to Mbps
+    download_speed = format_bytes(st.download())
+    upload_speed = format_bytes(st.upload())
 
-    return (f'[blue]Download speed: {download_speed:.2f} Mbps[/blue]\n'
-            f'[magenta]Upload speed: {upload_speed:.2f} Mbps[/magenta]')
+    return (f'[blue]Download speed: {download_speed}ps[/blue]',
+            f'[magenta]Upload speed: {upload_speed}ps[/magenta]')
 
 
 def main():
@@ -30,11 +39,17 @@ def main():
     console = Console()
 
     with console.status("[bold green]Checking internet speed...\n") as status:
-        speed_result = check_speed()
-        ping_result = check_ping(host_to_ping)
-        status.stop()
+        try:
+            download_speed, upload_speed = check_speed()
+            ping_result = check_ping(host_to_ping)
+        except:
+            console.print_exception()
+            return
+        finally:
+            status.stop()
         print(ping_result)
-        print(speed_result)
+        print(download_speed)
+        print(upload_speed)
 
 
 if __name__ == "__main__":
